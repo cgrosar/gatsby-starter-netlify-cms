09a159a2d205d376428e44f72104d7188c938814
cgrosar <45520058+cgrosar@users.noreply.github.com>
2018-12-01 19:34:52 +0000

    Revert "Cgrosar patch 1"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-start">
-        <Link className="navbar-item" to="/">
-          Home
-        </Link>
         <Link className="navbar-item" to="/about">
           About
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Products
         </Link>
       </div>
     </div>
