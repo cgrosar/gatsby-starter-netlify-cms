33644862ffabec43ba187e2b771fc88b505f9b6b
cgrosar <cgrosar@gmail.com>
2019-01-11 00:29:05 +0000

    Fix links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,13 +7,13 @@ const Navbar = () => (
       <Link className="navbar-item" to="/">
         Home
       </Link>
-      <Link className="navbar-item" to="/article/2018-12-17-links">
+      <Link className="navbar-item" to="/article/2017-12-17-links">
         Links
       </Link>
       <Link className="navbar-item" to="/article/2017-12-17-guidelines">
         Guidelines
       </Link>
-      <Link className="navbar-item" to="/article/2019-12-17-journal-club">
+      <Link className="navbar-item" to="/article/2017-12-17-journal-club">
         Journal club
       </Link>
       <Link className="navbar-item" to="/about">
